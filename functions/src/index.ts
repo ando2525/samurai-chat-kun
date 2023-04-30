37d31ac122b4f110b0a6f68d96f7b86a5ea32336
{ando} <{ando.pro.business@gmail.com}>
2023-05-01 02:30:54 +0900

    GPTResponse

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,40 +1,72 @@
 import * as functions from "firebase-functions";
-import { WebhookEvent } from "@line/bot-sdk";
+// import { WebhookEvent } from "@line/bot-sdk";
+import { GPTMessage } from "./types/GPTMessage";
+import axios from "axios";
 const https = require("https");
 
 const channelAccessToken =
   "7nEpdJkY/Ec2+QcJHyaR+vqUxf7chJmEF2OYYSbVh5wZvNvku6UgPIyIoEvy51KkCv0fLsl1Fogm0wrlwUWJrk1FrDWsCtoVM0jN6jxHab9mxGiD5nXf3zDTRlaoXlLOP9+UK1aHuuSGVevrJizYCQdB04t89/1O/w1cDnyilFU=";
+const chatGPTAPIKey = "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";
 
-exports.helloWorld = functions.https.onRequest((req, res) => {
+exports.helloWorld = functions.https.onRequest(async (req, res) => {
   if (req.method == "POST") {
-    const url = "https://api.line.me/v2/bot/message/reply";
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${channelAccessToken}`,
-      },
-    };
-
-    req.body["events"].forEach((event: WebhookEvent) => {
+    for (const event of req.body.events) {
       switch (event.type) {
         case "message":
           if (event.message.type === "text") {
-            const data = JSON.stringify({
-              replyToken: event.replyToken,
-              messages: [
-                {
-                  type: "text",
-                  text: event.message.text,
-                },
-              ],
-            });
-
-            const line = https.request(url, options);
-            line.write(data);
-            line.end();
+            const gptResponse = await chatGPTResponse(event.meassage.text);
+            sendLineMessage(event.replyToken, gptResponse);
           }
       }
-    });
+    }
   }
 });
+
+const sendLineMessage = (replyToken: string, message: string) => {
+  const url = "https://api.line.me/v2/bot/message/reply";
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${channelAccessToken}`,
+    },
+  };
+
+  const data = JSON.stringify({
+    replyToken: replyToken,
+    messages: [
+      {
+        type: "text",
+        text: message,
+      },
+    ],
+  });
+
+  const line = https.request(url, options);
+  line.write(data);
+  line.end();
+};
+
+const chatGPTResponse = async (message: string) => {
+  const url = "https://api.openai.com/v1/chat/completions";
+  const messages: GPTMessage[] = [
+    {
+      role: "system",
+      content:
+        "あなたは5歳の幼児です。語尾を「でちゅ」にして、ユーザーの相談を解決してください",
+    },
+    { role: "user", content: message },
+  ];
+
+  const body = JSON.stringify({
+    messages,
+    model: "gpt-3.5-turbo",
+  });
+  const res = await axios.post(url, body, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${chatGPTAPIKey}`,
+    },
+  });
+  return res.data.choices[0].message.content.trim();
+};
---
+++ functions/src/types/GPTMessage.ts
@@ -0,0 +1,4 @@
+export type GPTMessage = {
+  role: "user" | "system" | "assistant";
+  content: string;
+};
