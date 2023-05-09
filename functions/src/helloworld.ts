c1f6ac83226dcdbd7f5dee0fb374eb7e5ea07783
{ando} <{ando.pro.business@gmail.com}>
2023-05-10 01:47:38 +0900

    修正

--- functions/src/helloworld.ts
+++ functions/src/helloworld.ts
@@ -1,23 +1,52 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import { Message, WebhookEvent } from "@line/bot-sdk";
 import { GPTMessage } from "./types/GPTMessage";
 import axios from "axios";
-const LINE_CHANNEL_ACCESS_TOKEN =
-  "7nEpdJkY/Ec2+QcJHyaR+vqUxf7chJmEF2OYYSbVh5wZvNvku6UgPIyIoEvy51KkCv0fLsl1Fogm0wrlwUWJrk1FrDWsCtoVM0jN6jxHab9mxGiD5nXf3zDTRlaoXlLOP9+UK1aHuuSGVevrJizYCQdB04t89/1O/w1cDnyilFU=";
-const OPENAI_API_KEY = "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";
-const OPENAI_API_ENDPOINT =
-  "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions";
-const LINE_API_ENDPOINT = "https://api.line.me/v2/bot/message/reply";
+import {
+  LINE_API_ENDPOINT,
+  LINE_CHANNEL_ACCESS_TOKEN,
+  OPENAI_API_ENDPOINT,
+  OPENAI_API_KEY,
+} from "./constants";
+
+const IMAGE_URL =
+  "https://firebasestorage.googleapis.com/v0/b/samurai-chat-kun-398a4.appspot.com/o/gsap-greensock.svg?alt=media&token=f78fa587-5d50-458c-9e6c-c95b66e0388d";
 
 export const helloWorld = functions.https.onRequest(async (req, res) => {
   if (req.method == "POST") {
-    for (const event of req.body.events) {
+    const events: WebhookEvent[] = req.body.events;
+    for (const event of events) {
       switch (event.type) {
         case "message":
           if (event.message.type === "text") {
+            if (event.message.text === "画像") {
+              await sendLineMessage(event.replyToken, [
+                {
+                  type: "image",
+                  originalContentUrl: IMAGE_URL,
+                  previewImageUrl: IMAGE_URL,
+                  quickReply: {
+                    items: [
+                      {
+                        type: "action",
+                        action: {
+                          type: "message",
+                          label: "Yesを送信",
+                          text: "Yes",
+                        },
+                      },
+                    ],
+                  },
+                },
+              ]);
+            }
+
             const gptReponse = await getChatGPTResponse(event.message.text);
             await saveToFirestore(event.message.text, gptReponse);
-            await sendLineMessage(event.replyToken, gptReponse);
+            await sendLineMessage(event.replyToken, [
+              { type: "text", text: gptReponse },
+            ]);
           }
       }
     }
@@ -34,7 +63,7 @@ const saveToFirestore = async (message: string, gptReponse: string) => {
   });
 };
 
-const sendLineMessage = async (replyToken: string, message: string) => {
+const sendLineMessage = async (replyToken: string, messages: Message[]) => {
   const config = {
     headers: {
       "Content-Type": "application/json",
@@ -44,12 +73,7 @@ const sendLineMessage = async (replyToken: string, message: string) => {
 
   const data = JSON.stringify({
     replyToken,
-    messages: [
-      {
-        type: "text",
-        text: message,
-      },
-    ],
+    messages: messages,
   });
 
   await axios.post(LINE_API_ENDPOINT, data, config);
@@ -71,8 +95,8 @@ const getChatGPTResponse = async (message: string) => {
   let messages: GPTMessage[] = [
     {
       role: "system",
-      content:
-        "あなたは江戸時代の侍です。ユーザーの相談を侍らしく解決してください。",
+      content: "",
+      // "あなたは江戸時代の侍です。ユーザーの相談を侍らしく解決してください。",
     },
   ];
 
@@ -99,5 +123,5 @@ const getChatGPTResponse = async (message: string) => {
 
   const response = await axios.post(OPENAI_API_ENDPOINT, data, config);
 
-  return response.data.choices[0].message.content.trim();
+  return response.data.choices[0].message.content.trim() as string;
 };
